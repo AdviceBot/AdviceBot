d629f4e769d4be5ef58988f2118f8c26038adfb6
Adam Rybinski <arybinski@ibm.com>
2019-05-13 16:00:49 +0200

    Add skill-trip to source

--- skills/help.js
+++ skills/help.js
@@ -2,7 +2,7 @@ const { content } = require('./../blocks/content');
 module.exports = function(controller, watsonMiddleware) {
 
     controller.hears(['help'], ['direct_message', 'direct_mention', 'mention'], watsonMiddleware.hear, function(bot, message) {
-        bot.reply(message, content(message.watsonData.output.text.join('\n')));
+        bot.reply(message, message.watsonData.output.text.join('\n'));
 
         console.log('user requested help');
     });
